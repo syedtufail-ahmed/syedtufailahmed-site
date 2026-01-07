a00eb63ee4092a391115f0ca901b5ed2e48d424c
syedtufail-ahmed <syed@syedtufailahmed.com>
2026-01-08 00:44:43 +0300

    APP corrected

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./App";
+import  App from "./App";
 
 import "./styles/theme.css";
 import "./styles/typography.css";
