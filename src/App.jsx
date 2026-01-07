854632c6424186f9fde48fbec63aa65690d1d74d
syedtufail-ahmed <syed@syedtufailahmed.com>
2026-01-08 00:40:30 +0300

    corrected finallty

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Writing from "./pages/Writing";
 import HumanInTheLoop from "./pages/HumanInTheLoop";
 import HumanInTheLoopAIGovernance from "./pages/writing/HumanInTheLoopAIGovernance";
-import AnalyticsTracker from "./src/AnalyticsTracker";
+import AnalyticsTracker from "./AnalyticsTracker";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
+      <AnalyticsTracker />
+
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="writing" element={<Writing />} />
           <Route path="writing/human-in-the-loop" element={<HumanInTheLoop />} />
-          <Route path="/writing/human-in-the-loop-ai-governance" element={<HumanInTheLoopAIGovernance />}/>
+          <Route
+            path="writing/human-in-the-loop-ai-governance"
+            element={<HumanInTheLoopAIGovernance />}
+          />
         </Route>
-        <AnalyticsTracker />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
