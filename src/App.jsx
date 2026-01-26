467d3959d49625e11bdd80555e2763b12a96f730
syedtufail-ahmed <syed@syedtufailahmed.com>
2026-01-27 00:24:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Writing from "./pages/Writing";
 import HumanInTheLoop from "./pages/HumanInTheLoop";
 import HumanInTheLoopAIGovernance from "./pages/writing/HumanInTheLoopAIGovernance";
-import AnalyticsTracker from "./AnalyticsTracker";
-import Book from "./pages/Book";
 import ExecutiveCredentials from "./pages/ExecutiveCredentials";
+import Book from "./pages/Book";
+
+import AnalyticsTracker from "./AnalyticsTracker";
 
 export default function App() {
   return (
     <>
+      {/* GA SPA tracking */}
       <AnalyticsTracker />
 
       <Routes>
         <Route element={<Layout />}>
+
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="writing" element={<Writing />} />
-          <Route path="writing/human-in-the-loop" element={<HumanInTheLoop />} />
-          <Route path="/executive-credentials" element={<ExecutiveCredentials />} />
-          <Route path="writing/human-in-the-loop-ai-governance" element={<HumanInTheLoopAIGovernance />}
-            <Route path="/Book" element={<Book />} />
+
+          <Route
+            path="writing/human-in-the-loop"
+            element={<HumanInTheLoop />}
           />
+
+          <Route
+            path="writing/human-in-the-loop-ai-governance"
+            element={<HumanInTheLoopAIGovernance />}
+          />
+
+          <Route
+            path="executive-credentials"
+            element={<ExecutiveCredentials />}
+          />
+
+          <Route
+            path="book"
+            element={<Book />}
+          />
+
         </Route>
       </Routes>
     </>
   );
 }
-
