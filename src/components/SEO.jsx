c087280db64c6bcf803904807af5e19b0134d0fe
syedtufail-ahmed <syed@syedtufailahmed.com>
2026-01-28 21:42:55 +0300

    Create SEO.jsx

---
+++ src/components/SEO.jsx
@@ -0,0 +1,32 @@
+import { Helmet } from "react-helmet-async";
+
+export default function SEO({
+  title,
+  description,
+  url,
+  image,
+  type = "website"
+}) {
+  return (
+    <Helmet>
+      {/* Primary Meta */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* Open Graph */}
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      {/* Twitter / X */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+    </Helmet>
+  );
+}
