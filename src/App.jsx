be71d93ea57689b03fdc6ffd2859fcea9402377d
syedtufail-ahmed <syed@syedtufailahmed.com>
2026-06-28 00:12:36 +0300

    Refactor routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,51 +6,28 @@ import About from "./pages/About";
 import Writing from "./pages/Writing";
 import HumanInTheLoop from "./pages/HumanInTheLoop";
 import HumanInTheLoopAIGovernance from "./pages/writing/HumanInTheLoopAIGovernance";
+import CulturalAccountabilityAIGovernance from "./pages/writing/CulturalAccountabilityAIGovernance";
 import ExecutiveCredentials from "./pages/ExecutiveCredentials";
 import Book from "./pages/Book";
 import Speaking from "./pages/Speaking";
 
 import AnalyticsTracker from "./AnalyticsTracker";
-import CulturalAccountabilityAIGovernance from "./pages/writing/CulturalAccountabilityAIGovernance";
+
 export default function App() {
   return (
     <>
-      {/* GA SPA tracking */}
       <AnalyticsTracker />
-
       <Routes>
         <Route element={<Layout />}>
-
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="writing" element={<Writing />} />
-
-          <Route
-            path="writing/human-in-the-loop"
-            element={<HumanInTheLoop />}
-          />
-
-          <Route
-            path="writing/human-in-the-loop-ai-governance"
-            element={<HumanInTheLoopAIGovernance />}
-          />
-
-          <Route
-            path="ExecutiveCredentials"
-            element={<ExecutiveCredentials />}
-          />
-
-          <Route
-            path="book"
-            element={<Book />}
-          />
-
-          <Route
-            path="speaking"
-            element={<Speaking />}
-          />
-// then inside your routes:
-<Route path="writing/cultural-accountability-ai-governance" element={<CulturalAccountabilityAIGovernance />} />
+          <Route path="writing/human-in-the-loop" element={<HumanInTheLoop />} />
+          <Route path="writing/human-in-the-loop-ai-governance" element={<HumanInTheLoopAIGovernance />} />
+          <Route path="writing/cultural-accountability-ai-governance" element={<CulturalAccountabilityAIGovernance />} />
+          <Route path="executive-credentials" element={<ExecutiveCredentials />} />
+          <Route path="book" element={<Book />} />
+          <Route path="speaking" element={<Speaking />} />
         </Route>
       </Routes>
     </>
